5595d59449f1076fb40c549649febc20d096ebe8
Rarai365 <rajeshbantawarai90@gmail.com>
2025-01-04 09:52:51 +1100

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const strt = document.getElementById("strt");
+const strtNum = document.getElementById("strtNum");
+const pstlCode = document.getElementById("pstlCode");
+const city = document.getElementById("city");
+const state = document.getElementById("state");
+const country = document.getElementById("country");
+// const btn1 = document.getElementById("btn1");
+// const btn2 = document.getElementById("btn2");
+const outputField = document.getElementById("outputField");
+
+function fun1() {
+  outputField.innerHTML =
+    "Thank you for providing your home Adress:" +
+    "<br><br>" +
+    strtNum.value +
+    " " +
+    strt.value +
+    ", " +
+    city.value +
+    " " +
+    pstlCode.value +
+    ", " +
+    state.value +
+    ", " +
+    country.value;
+}
+
+// // btn2.onclick = () => document.getElementById("container").requestFullscreen();
+function fun2() {
+  document.getElementById("container").reset();
+}
